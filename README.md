c0f3c5969302d72c0296e6e32e19a6fb5e2c2c01
MikelSV <mikel.san@educa.madrid.org>
2024-12-03 14:13:05 +0100

    enunciado

---
+++ README.md
@@ -0,0 +1,10 @@
+# Navegación 1
+
+Configura la navegación de este proyecto siguiendo estas normas:
+
+- La pantalla inicial debe ser Screen1, se cargará al ejecutar la MainActivity
+- De Screen1 se puede navegar a Screen2 y a Screen3 con los botones
+- Desde Screen2 se puede navegar a Screen3 o volver a la pantalla anterior
+- Desde Screen3 solo se puede volver a la pantalla anterior
+
+
---
+++ app/src/main/java/com/example/navigation1/presentation/ui/screens/Screen3.kt
@@ -0,0 +1,36 @@
+package com.example.navigation1.presentation.ui.screens
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Screen3() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Pantalla 3") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Button(onClick = { /* Vuelve a la pantalla anterior */ }) {
+                Text(text = "Volver")
+            }
+        }
+    }
+}
